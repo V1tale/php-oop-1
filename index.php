22649f3d4914422b5ce67f4740122440cc82fb42
V1tale <elativ.c@gmail.com>
2022-12-07 15:49:09 +0100

    implementazione multi genere nei movies

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 require_once __DIR__ . "/models/movie.php";
 
-$forrestGump = new Movie("Forrest Gump", "drama", "english", "1994", "2.25hr", "", "");
+$genres = [
+    new genre("drama"),
+    new genre("comedy")
+];
+$forrestGump = new Movie("Forrest Gump", $genres, "english", "1994", "2.25hr", "", "");
 var_dump($forrestGump);
 $theTrumanShow = new Movie("The Truman Show", "comedy", "english", "1998", "1.43hr", "", "");
 var_dump($theTrumanShow);
 $deadpool = new Movie("Deadpool", "action", "english", "2016", "2.05hr", "", "");
 var_dump($deadpool);
 ?>
-
--- models/movie.php
+++ models/movie.php
@@ -3,16 +3,17 @@
 class Movie {
     public $name;
     public $duration;
-    public $genre;
+    public $genres;
     public $year;
     public $language;
     public $cast;
     public $description;
+    
 
-    function __construct($name, $genre, $language, $year, $duration, $cast, $description) {
+    function __construct( string $name, array $genres, string $language, string $year, string $duration, string $cast, string $description) {
 
         $this->name = $name;
-        $this-> genre = $genre;
+        $this-> genres = $genres;
         $this->language = $language;
         $this->year =$year;
         $this->duration = $duration;
